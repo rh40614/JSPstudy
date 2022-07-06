cd19dbbec0ab47497423fedcda4aa6021ba1469d
APplE <107603870+rh40614@users.noreply.github.com>
2022-07-06 10:22:46 +0900

    Delete mysqlProject directory

--- mysqlProject/src/main/java/mysqlProject/Utility.java
+++
@@ -1,7 +0,0 @@
-package mysqlProject;
-
-public class Utility {
-	public static String toJSON(String source) {
-		return source.replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", "\\n").replace("\r\n", "\\n");
-	}
-}
